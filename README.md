90603c0f230dfc288f4dff85d02623e73833ad6b
Nick <nanthony007@gmail.com>
2020-02-23 21:22:56 -0500

    Insert comments on functions

---
+++ README.md
@@ -0,0 +1,3 @@
+This project is to create a simple website with an API 
+component for wrestler statistics and is under active development, 
+check back frequently for updates.
--- app.py
+++ app.py
@@ -3,13 +3,14 @@ from flask import Flask
 from flask import render_template
 from flask_sqlalchemy import SQLAlchemy
 
+# Configuration lines
 app = Flask(__name__)
 db_path = os.path.join(os.path.dirname(__file__), 'database.db')
 db_uri = 'sqlite:///{}'.format(db_path)
 app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
 db = SQLAlchemy(app)
 
-
+# User table with fields
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
@@ -18,7 +19,7 @@ class User(db.Model):
     def __repr__(self):
         return '<User %r>' % self.username
 
-
+# Home page route, renders home.html template
 @app.route('/')
 def home():
     return render_template('home.html')
