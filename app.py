72eba4b5d5bce663cae157b7cf4a98f452e32a09
Nick <nanthony007@gmail.com>
2020-02-28 14:01:15 -0500

    Week 3, added more tempalates, added 3 User accounts

--- app.py
+++ app.py
@@ -8,8 +8,30 @@ app = Flask(__name__)
 db_path = os.path.join(os.path.dirname(__file__), 'database.db')
 db_uri = 'sqlite:///{}'.format(db_path)
 app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
+app.config["TEMPLATES_AUTO_RELOAD"] = True
 db = SQLAlchemy(app)
 
+
+### ROUTES SECTION
+# Home page route, renders home.html template
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/usage')
+def usage():
+    return render_template('usage.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+
+### MODELS SECTION
 # User table with fields
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -18,9 +40,3 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User %r>' % self.username
-
-# Home page route, renders home.html template
-@app.route('/')
-def home():
-    return render_template('home.html')
-
