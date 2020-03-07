f367ba562e09ddfb4a2d3768ad8462bef0cb9cb4
Nick <nanthony007@gmail.com>
2020-03-07 13:54:17 -0500

    Week 4. Adds login/logout functionality into site.  Adds styling on entire site and content on Home/About pages.

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 from flask import Flask
-from flask import render_template
+from flask import flash, g, redirect, render_template, request, session, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 # Configuration lines
@@ -9,26 +9,9 @@ db_path = os.path.join(os.path.dirname(__file__), 'database.db')
 db_uri = 'sqlite:///{}'.format(db_path)
 app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
 app.config["TEMPLATES_AUTO_RELOAD"] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SECRET_KEY"] = 'dev'
 db = SQLAlchemy(app)
-
-
-### ROUTES SECTION
-# Home page route, renders home.html template
-@app.route('/')
-def home():
-    return render_template('home.html')
-
-@app.route('/about')
-def about():
-    return render_template('about.html')
-
-@app.route('/usage')
-def usage():
-    return render_template('usage.html')
-
-@app.route('/contact')
-def contact():
-    return render_template('contact.html')
 
 
 ### MODELS SECTION
@@ -40,3 +23,66 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User %r>' % self.username
+
+
+### ROUTES SECTION
+# Home page route, renders home.html template
+@app.route('/', methods=('POST', 'GET'))
+def home():
+    if request.method == 'POST':
+        username = request.form['username']
+        user = User.query.filter_by(username=username).first()
+
+        if user is not None:
+            session.clear()
+            session['user_id'] = user.id
+            flash('Login succesful.', 'success')
+            return redirect(url_for('home'))
+
+        elif user is None:
+            flash('Invalid username. Please try again.', 'danger')
+            return render_template('home.html')
+
+    elif request.method == 'GET':
+        user_id = session.get('user_id')
+        if user_id is None:
+            g.user = None
+        else:
+            g.user = User.query.filter_by(id=user_id).first()
+        return render_template('home.html')
+
+@app.route('/about')
+def about():
+    user_id = session.get('user_id')
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = User.query.filter_by(id=user_id).first()
+    return render_template('about.html')
+
+@app.route('/usage')
+def usage():
+    user_id = session.get('user_id')
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = User.query.filter_by(id=user_id).first()
+    return render_template('usage.html')
+
+@app.route('/contact')
+def contact():
+    user_id = session.get('user_id')
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = User.query.filter_by(id=user_id).first()
+    return render_template('contact.html')
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    flash('Logout successful', 'success')
+    return render_template('logout.html')
+
+
+
